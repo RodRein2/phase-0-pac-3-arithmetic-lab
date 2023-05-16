0f63ccc383a6add2240484f157d27261517a5393
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-05-15 17:55:03 -0700

    commit lab

---
+++ index.js
@@ -0,0 +1,54 @@
+function add() {
+    return "something";
+}
+function subtract() {
+    return "something";
+}
+function multiply() {
+    return "something";
+}
+function divide() {
+    return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a +b;
+}
+function subtract(a, b) {
+    return "something";
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return "something";
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return "something";
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return "something";
+}
+function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+    return "something";
+}
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
